f459f1d3eb64a0cd62055fcbf17473ed70aef546
PacoReinaCampo <frareicam@gmail.com>
2021-02-15 22:36:02 +0100

    Working in Peripheral-MSI

--- sim/verilog/tests/core/ahb3/verilator/simulate.bat
+++ sim/verilog/tests/core/ahb3/verilator/simulate.bat
@@ -1,5 +1,5 @@
 @echo off
 call ../../../../../../settings64_verilator.bat
 
-verilator -Wno-lint +incdir+../../../../../../rtl/verilog/ahb3/pkg --cc -f system.vc --top-module mpsoc_msi_testbench
+verilator -Wno-lint --cc -f system.vc --top-module mpsoc_msi_testbench
 pause
