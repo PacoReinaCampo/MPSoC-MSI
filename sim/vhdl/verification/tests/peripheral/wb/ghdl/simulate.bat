8f6ca745134c7f6fb6d7ce915288d28d757ade0b
PacoReinaCampo <frareicam@gmail.com>
2023-04-12 16:44:25 +0200

    Working in Peripheral-MSI 🔑

--- sim/vhdl/verification/tests/peripheral/wb/ghdl/simulate.bat
+++ sim/vhdl/verification/tests/peripheral/wb/ghdl/simulate.bat
@@ -45,7 +45,9 @@
 @echo off
 call ../../../../../../../settings64_ghdl.bat
 
-ghdl -a --std=08 ../../../../../../../rtl/vhdl/code/pkg/peripheral/wb/peripheral_msi_pkg_wb.vhd
+ghdl -a --std=08 ../../../../../../../rtl/vhdl/code/pkg/core/vhdl_pkg.vhd
+ghdl -a --std=08 ../../../../../../../rtl/vhdl/code/pkg/peripheral/wb/peripheral_wb_pkg.vhd
+
 ghdl -a --std=08 ../../../../../../../rtl/vhdl/code/peripheral/wb/arbiter/peripheral_msi_arbiter.vhd
 ghdl -a --std=08 ../../../../../../../rtl/vhdl/code/peripheral/wb/cdc/peripheral_msi_cc561_wb.vhd
 ghdl -a --std=08 ../../../../../../../rtl/vhdl/code/peripheral/wb/cdc/peripheral_msi_cdc_wb.vhd
@@ -54,7 +56,9 @@ ghdl -a --std=08 ../../../../../../../rtl/vhdl/code/peripheral/wb/main/periphera
 ghdl -a --std=08 ../../../../../../../rtl/vhdl/code/peripheral/wb/main/peripheral_msi_data_resize_wb.vhd
 ghdl -a --std=08 ../../../../../../../rtl/vhdl/code/peripheral/wb/main/peripheral_msi_interface_wb.vhd
 ghdl -a --std=08 ../../../../../../../rtl/vhdl/code/peripheral/wb/main/peripheral_msi_mux_wb.vhd
+
 ghdl -a --std=08 ../../../../../../../bench/vhdl/code/tests/peripheral/wb/peripheral_msi_testbench.vhd
+	
 ghdl -m --std=08 peripheral_msi_testbench
 ghdl -r --std=08 peripheral_msi_testbench --ieee-asserts=disable-at-0 --disp-tree=inst > peripheral_msi_testbench.tree
 pause
